a39ec618eba6e4660b78cd009e8abb6fca3243ae
Denitsa Marinova <53836946+DenMarinova@users.noreply.github.com>
2020-03-29 23:09:17 +0300

    home page and posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,67 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+// const path = require(`path`)
+// const { slash } = require(`gatsby-core-utils`)
 
-// You can delete this file if you're not using it
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
+
+//   // query content for WordPress posts
+//   const result = await graphql(`
+//     query {
+//       allWordpressPost {
+//         edges {
+//           node {
+//             id
+//             slug
+//           }
+//         }
+//       }
+//     }
+//   `)
+
+//   const postTemplate = path.resolve(`./src/templates/post.js`)
+//   result.data.allWordpressPost.edges.forEach(edge => {
+//     createPage({
+//       // will be the url for the page
+//       path: `/post/${edge.node.slug}/`,
+//       // specify the component template of your choice
+//       component: slash(postTemplate),
+//       // In the ^template's GraphQL query, 'id' will be available
+//       // as a GraphQL variable to query for this posts's data.
+//       context: {
+//         id: edge.node.id,
+//       },
+//     })
+//   })
+// }
+
+const path = require(`path`)
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      allWordpressPost(sort: { fields: [date] }) {
+        edges {
+          node {
+            title
+            excerpt
+            content
+            slug
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allWordpressPost.edges.forEach(({ node }) => {
+      createPage({
+        path: node.slug,
+        component: path.resolve(`./src/templates/post.js`),
+        context: {
+          // This is the $slug variable
+          // passed to blog-post.js
+          slug: node.slug,
+        },
+      })
+    })
+  })
+}
---
+++ src/templates/post.js
@@ -0,0 +1,62 @@
+// import React, { Component } from "react"
+// import PropTypes from "prop-types"
+// import Img from "gatsby-image"
+
+// class PostTemplate extends Component {
+//     render() {
+//         const post = this.props.data.wordpressPost
+
+//         return (
+//             <div>
+//                 <h1 dangerouslySetInnerHTML={{ __html: post.title }} />
+//                 <div dangerouslySetInnerHTML={{ __html: post.content }} />
+//             </div>
+//         )
+//     }
+// }
+
+
+// export default PostTemplate
+
+// export const pageQuery = graphql`
+//     query currentPostQuery($id: String!) {
+//         wordpressPost(id: { eq: $id }) {
+//             title
+//             content
+//         }
+//         site {
+//             siteMetadata {
+//                 title
+              
+//             }
+//         }
+//     }`
+
+import React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
+
+export default ({ data }) => {
+  const post = data.allWordpressPost.edges[0].node
+  console.log(post)
+  return (
+    <Layout>
+      <div>
+        <h1>{post.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+      </div>
+    </Layout>
+  )
+}
+export const query = graphql`
+  query($slug: String!) {
+    allWordpressPost(filter: { slug: { eq: $slug } }) {
+      edges {
+        node {
+          title
+          content
+        }
+      }
+    }
+  }
+`
